6d7dd414af9d26cfb3b9fb2b1526573e67484d28
Parlinggoman <parlinggomanpakpahan055@gmail.com>
2021-08-30 23:15:34 +0700

    Create University.cs

---
+++ NETcore/NETcore/Model/University.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETcore.Model
+{
+    [Table("tb-m-University")]
+    public class University
+    {
+        public int UniversityId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        // one to many with education 
+        public virtual ICollection<Education> Educations { get; set; }
+
+
+    }
+}
