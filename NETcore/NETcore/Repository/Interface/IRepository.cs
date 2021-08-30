f040f177adafa83fc9f572a1583f487a049f4cdf
Parlinggoman <parlinggomanpakpahan055@gmail.com>
2021-08-30 23:13:26 +0700

    Create IRepository.cs

---
+++ NETcore/NETcore/Repository/Interface/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using NETcore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETcore.Repository.Interface
+{
+
+    public  interface IRepository<Entity,Key> where Entity: class
+    {
+        IEnumerable<Entity> Get();
+        Person Get(Key key);
+        int Insert(Entity entity);
+        int Update(Entity entity); 
+        int Delete(Key key);
+    }
+}
