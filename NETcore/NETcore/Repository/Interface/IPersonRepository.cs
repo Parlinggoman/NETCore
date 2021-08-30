7387729c91f92020e156ae7739f2f0881f7e7624
Parlinggoman <parlinggomanpakpahan055@gmail.com>
2021-08-30 23:13:29 +0700

    Create IPersonRepository.cs

---
+++ NETcore/NETcore/Repository/Interface/IPersonRepository.cs
@@ -0,0 +1,17 @@
+﻿using NETcore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETcore.Repository.Interface
+{
+  interface IPersonRepository
+    {
+        IEnumerable<Person> Get();
+        Person Get(string NIK);
+        int Insert(Person person);
+        int Update( Person person); // klo mau update dapat dilakukan dari id/NIK jadi cukup masukin variabel id nya saja/primary key
+        int Delete(string NIK); 
+    }
+}
