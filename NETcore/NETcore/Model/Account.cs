ffca43b5744a4fb20fe09ad5a0ec6ff6ee81601d
Parlinggoman <parlinggomanpakpahan055@gmail.com>
2021-08-30 23:16:06 +0700

    Create Account.cs

---
+++ NETcore/NETcore/Model/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace NETcore.Model
+{
+    [Table("tb-m-Account")]
+    public class Account
+    {
+        [Key]
+        public string NIK { get; set; }
+        public string Password { get; set; }
+
+        [JsonIgnore]
+        public virtual Person Person { get; set; }
+        [JsonIgnore]
+        public virtual Profiling Profiling { get; set; }
+
+    }
+}
